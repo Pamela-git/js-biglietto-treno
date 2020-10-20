03006b4bfc828aea8a279605b2ee5af81aa430ef
Pamela Ladu <pamela.ladu@gmail.com>
2020-10-20 14:37:31 +0200

    scrittura commenti

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//calcolare prezzo biglietto in base all'età e ai km
+/*il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65*/
+
+//step 1: chiedere numero di km percorsi
+
+//chiedere età del passeggero
+
+//calcolo prezzo Biglietto generico
+
+//calcolo prezzo biglietto con relative condizioni
